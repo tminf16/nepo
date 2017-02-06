5c49bda96722a1886b4aa10fe03b20318bd08e7a
sascha <saschaweisenberger@live.de>
2017-02-06 16:52:22 +0100

    Added "CurveRule"

--- Nepo/Common/AgentConfig.cs
+++ Nepo/Common/AgentConfig.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using Nepo.Common.Rules;
+using System.Collections.Generic;
 
 namespace Nepo.Common
 {
     public class AgentConfig
     {
         public List<Layer> Layers { get; set; }
+        public List<ITargetFunctionComponent> Rules { get; set; } 
     }
 }
